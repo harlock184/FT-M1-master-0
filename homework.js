d905f56720464d4dfb63b466ace72d360739cb1d
harlock184 <105575194+harlock184@users.noreply.github.com>
2023-02-22 12:58:25 -0600

    homework1

---
+++ homework.js
@@ -0,0 +1,17 @@
+'use strict';
+
+function BinarioADecimal(num) {
+
+   var decimal = 0
+   for(var i = 0; i<num.length; i++){
+      decimal = decimal + num[i] * 2**(num.length-1-i)
+   }
+   return decimal
+}
+
+function DecimalABinario(num) {}
+
+module.exports = {
+   BinarioADecimal,
+   DecimalABinario,
+};
