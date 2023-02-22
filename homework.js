310e18ccd914a1471f47a83c9f506db7d6d0453b
harlock184 <105575194+harlock184@users.noreply.github.com>
2023-02-22 16:51:24 -0600

    Violeta

--- homework.js
+++ homework.js
@@ -6,10 +6,14 @@ function BinarioADecimal(num) {
    for(var i = 0; i<num.length; i++){
       decimal = decimal + num[i] * 2**(num.length-1-i)
    }
-   return decimal.toExponential.
+   return decimal
 }
 
-function DecimalABinario(num) {}
+function DecimalABinario(num) {
+
+  var binario=  num.toString(2);
+  return binario
+}
 
 module.exports = {
    BinarioADecimal,
